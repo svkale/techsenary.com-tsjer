c9010d6ec8b99e337673e499a74eddda3c33474b
Siddharth Kale <organizer.sites@gmail.com>
2020-11-30 21:56:56 +0530

    v1

--- js/design.js
+++ js/design.js
@@ -1,7 +1,6 @@
 if(document.getElementsByClassName('background_img')[0])
 {
 	var bgimages=document.getElementsByClassName('background_img');
-	console.log(bgimages);
 	for(var i=0;i<bgimages.length;i++)
 	{
 		bgimages[i].parentElement.style.position="relative";
@@ -35,18 +34,22 @@ for(var i=0;i<document.querySelectorAll("[data-ssheet-url]").length;i++)
 
 for(var i=0;i<document.getElementsByClassName("gdoc_active_page").length;i++)
 {
-	request(document.getElementsByClassName("gdoc_active_page")[i].getAttribute("data-gdoc-target-url"),"request_gdoc_show",document.getElementById(document.getElementsByClassName("gdoc_active_page")[i].parentElement.getAttribute("data-gdoc-target-ele-id")));
+	request(document.getElementsByClassName("gdoc_active_page")[i].getAttribute("data-gdoc-target-url"),"test",document.getElementById(document.getElementsByClassName("gdoc_active_page")[i].parentElement.getAttribute("data-gdoc-target-ele-id")));
 }
 
 var nav_elements=document.getElementsByTagName('main')[0].getElementsByTagName('nav')[0].getElementsByTagName('ul')[0].getElementsByTagName('li');
 for(var i=0;i<nav_elements.length;i++)
 {
-	console.log(nav_elements);
 	if(nav_elements[i].getAttribute("data-gdoc-target-url"))
 	{
 		nav_elements[i].addEventListener("click",function()
 		{
-			request(this.getAttribute("data-gdoc-target-url"),"request_gdoc_show",document.getElementById(this.parentElement.getAttribute("data-gdoc-target-ele-id")));
+			request(this.getAttribute("data-gdoc-target-url"),"test",document.getElementById(this.parentElement.getAttribute("data-gdoc-target-ele-id")));
 		});
 	}
+}
+
+function test(params)
+{
+
 }
