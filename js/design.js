a588da021df8cf3cc1a3a4b5c9277512ed717524
root <root@web-admin.local.lan>
2020-11-30 15:17:12 +0530

    v1

--- js/design.js
+++ js/design.js
@@ -9,17 +9,19 @@ if(document.getElementsByClassName('background_img')[0])
 }
 
 
-var nav_elements=document.getElementsByTagName('main')[0].getElementsByTagName('nav')[0].getElementsByTagName('ul')[0].getElementsByTagName('li');
-for(var i=0;i<nav_elements.length;i++)
-{
-	if(nav_elements[i].getAttribute("data-iframe-url"))
-	{
-		nav_elements[i].addEventListener("click",function()
-		{
-			window.location.href="https://"+this.getAttribute("data-iframe-url");
-		});
-	}
-}
+// var nav_elements=document.getElementsByTagName('main')[0].getElementsByTagName('nav')[0].getElementsByTagName('ul')[0].getElementsByTagName('li');
+// for(var i=0;i<nav_elements.length;i++)
+// {
+// 	if(nav_elements[i].getAttribute("data-iframe-url"))
+// 	{
+// 		nav_elements[i].addEventListener("click",function()
+// 		{
+// 			window.location.href="https://"+this.getAttribute("data-iframe-url");
+// 		});
+// 	}
+// }
+
+
 
 function put_hitcount(request_obj,params)
 {
@@ -29,7 +31,22 @@ for(var i=0;i<document.querySelectorAll("[data-ssheet-url]").length;i++)
 {
 	request(document.querySelectorAll("[data-ssheet-url]")[i].getAttribute("data-ssheet-url"),"put_hitcount",document.querySelectorAll("[data-ssheet-url]")[i]);
 }
-for(var i=0;i<document.querySelectorAll("[data-doc-url]").length;i++)
+
+
+for(var i=0;i<document.getElementsByClassName("gdoc_active_page").length;i++)
 {
-	request(document.querySelectorAll("[data-doc-url]")[i].getAttribute("data-doc-url"),"request_gdoc_show",document.querySelectorAll("[data-doc-url]")[i]);
+	request(document.getElementsByClassName("gdoc_active_page")[i].getAttribute("data-gdoc-target-url"),"request_gdoc_show",document.getElementById(document.getElementsByClassName("gdoc_active_page")[i].parentElement.getAttribute("data-gdoc-target-ele-id")));
+}
+
+var nav_elements=document.getElementsByTagName('main')[0].getElementsByTagName('nav')[0].getElementsByTagName('ul')[0].getElementsByTagName('li');
+for(var i=0;i<nav_elements.length;i++)
+{
+	console.log(nav_elements);
+	if(nav_elements[i].getAttribute("data-gdoc-target-url"))
+	{
+		nav_elements[i].addEventListener("click",function()
+		{
+			request(this.getAttribute("data-gdoc-target-url"),"request_gdoc_show",document.getElementById(this.parentElement.getAttribute("data-gdoc-target-ele-id")));
+		});
+	}
 }
