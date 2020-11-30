4f1eccb1caec919e62b4dd1d66bf51172cf894f3
root <root@web-admin.local.lan>
2020-11-30 11:58:06 +0530

    v1

--- js/design.js
+++ js/design.js
@@ -21,30 +21,6 @@ for(var i=0;i<nav_elements.length;i++)
 	}
 }
 
-
-function request(path,func)
-{
-	var param=[];
-	for(var i=2;i<arguments.length;i++)
-	{
-		param[i-2]=arguments[i];
-	}
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function()
-	{
-		if (this.readyState==4 && this.status==200)
-		{
-			window[func](this,param);
-		}
-	};
-	xmlhttp.open('GET',path,true); 
-	xmlhttp.send();
-	return;
-}
-function request_gsheet(request_obj)
-{
-	return JSON.parse(request_obj.responseText.substring(28,request_obj.responseText.length-2)).feed.entry;
-}
 function put_hitcount(request_obj,params)
 {
 	params[0].innerHTML=request_gsheet(request_obj)[0].content.$t;
@@ -52,33 +28,6 @@ function put_hitcount(request_obj,params)
 for(var i=0;i<document.querySelectorAll("[data-ssheet-url]").length;i++)
 {
 	request(document.querySelectorAll("[data-ssheet-url]")[i].getAttribute("data-ssheet-url"),"put_hitcount",document.querySelectorAll("[data-ssheet-url]")[i]);
-}
-function request_gdoc(request_obj)
-{
-	return request_obj.responseText;
-}
-function request_gdoc_show(request_obj,params)
-{
-	var put_target=params[0];
-	var response_doc=new DOMParser().parseFromString(request_gdoc(request_obj),"text/html");
-	var response_doc_headtags=response_doc.getElementsByTagName('head')[0].children;
-	var put_data="";
-	for(var i=0;i<response_doc_headtags.length;i++)
-	{
-		if(response_doc_headtags[i].tagName=="STYLE")
-		{
-			put_data+=response_doc_headtags[i].outerHTML;
-		}
-	}
-	put_data+=response_doc.getElementsByTagName('body')[0].outerHTML.replace(/body/g,"div");
-	put_target.insertAdjacentHTML("beforeend",put_data);
-	put_target.classList.add("gdoc_contents");
-}
-function show_gdoc(request_obj,params)
-{
-	var xmlDoc = new DOMParser().parseFromString(request_gdoc(request_obj),"text/html");
-	params[0].insertAdjacentHTML("beforeend",xmlDoc.getElementsByTagName('head')[0].innerHTML);
-	params[0].insertAdjacentHTML("beforeend",xmlDoc.getElementsByTagName('body')[0].innerHTML);
 }
 for(var i=0;i<document.querySelectorAll("[data-doc-url]").length;i++)
 {
